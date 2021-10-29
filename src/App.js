da40ff806c277e9dd40ac9b8dd9bfaa0ded81ad1
Mustafa Atay <ataymstf35@gmail.com>
2021-10-29 16:10:19 +0300

    setting up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Button, Typography } from '@mui/material';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#ff0000",
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route path="/about">
+              <Typography>about</Typography>
+              <Link to="/">go to root</Link>
+            </Route>
+            <Route path="/users">
+              <Typography>users</Typography>
+              <Link to="/">go to root</Link>
+            </Route>
+            <Route path="/">
+              <Typography>root</Typography>
+              <Link to="about">go to about</Link>
+              <Link to="users">go to users</Link>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
